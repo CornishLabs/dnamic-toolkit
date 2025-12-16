51ba8a977bf74c09dab62622c6d7b2df686e0abc
tomhepz <9285131+tomhepz@users.noreply.github.com>
2025-12-16 17:27:16 +0000

    Test loading in data

--- examples/load_and_plot.py
+++ examples/load_and_plot.py
@@ -1,6 +1,6 @@
-# from oitg.results import load_hdf5_file
 from dnamic_toolkit.results import load_hdf5_file
 import numpy as np
+import matplotlib.pyplot as plt
 
 fn = "/home/lab/dnamic-lab/results/2025-12-09/15/000000492-RabiFlopWithAnalysis.h5"   # <-- your file
 r = load_hdf5_file(fn)
@@ -13,9 +13,21 @@ print("\nDatasets:")
 for k, v in r["datasets"].items():
     arr = np.asarray(v)
     print(f"  {k:30s}  shape={arr.shape} dtype={arr.dtype}")
+    # print(arr)
 
 print("\nArchive entries:")
 for k, v in r["archive"].items():
     # archive items are often bytes/blobs
     print(f"  {k:30s}  type={type(v)}  len={len(v) if hasattr(v, '__len__') else '??'}")
 
+ys= r["datasets"]["ndscan.rid_492.points.channel_p"]
+xs= r["datasets"]["ndscan.rid_492.points.axis_0"]
+
+print(xs)
+print(ys)
+
+fig,ax = plt.subplots()
+
+ax.plot(xs,ys,ls='',marker='o')
+
+plt.show()
